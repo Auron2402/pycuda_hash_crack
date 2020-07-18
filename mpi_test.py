057b7a9133ddbdd82e0e82a9188de6990a7d9e24
christian_neuerer@web.de <Lu24ci02fer95>
2020-07-18 17:45:55 +0200

    added new requirements to requirements.txt

--- mpi_test.py
+++ mpi_test.py
@@ -6,12 +6,8 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 if rank == 0:
-    # with open('rockyou.txt', 'r', encoding='latin-1') as dictionary:
-    #     data = dictionary.readlines()
     complete_data = numpy.loadtxt('rockyou_smol.txt', dtype=str, delimiter='\n', encoding='latin-1')
     chunks = numpy.array_split(complete_data, size, axis=0)
-
-
 else:
     chunks = None
 
