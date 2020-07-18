1e16c546d04bddb8bcad92e4aef800efb885af09
christian_neuerer@web.de <Lu24ci02fer95>
2020-07-18 19:53:34 +0200

    changed reading to pandas reading added pandas to requirements.txt

--- mpi_test.py
+++ mpi_test.py
@@ -1,16 +1,32 @@
 from mpi4py import MPI
 import numpy
+import pandas as pd
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+# generate data
+
+
+
+
+# read data
 if rank == 0:
-    complete_data = numpy.loadtxt('rockyou_smol.txt', dtype=str, delimiter='\n', encoding='latin-1')
+    # rockyou has 14.344.395 lines
+    complete_data = pd.read_csv('rockyou.txt', sep="\n", encoding='latin-1', header=None, squeeze=True, error_bad_lines=False)
+else:
+    complete_data = None
+
+
+
+# read and split data
+if rank == 0:
     chunks = numpy.array_split(complete_data, size, axis=0)
 else:
     chunks = None
 
+#send data
 data = comm.scatter(chunks, root=0)
 print('rank', rank, 'has data:', len(data))
 
