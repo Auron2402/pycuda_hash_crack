1ccc3e6f2dc75535798f8d28366f360ff0645b51
christian_neuerer@web.de <Lu24ci02fer95>
2020-07-18 17:40:24 +0200

    first stable mpi version

--- mpi_test.py
+++ mpi_test.py
@@ -1,16 +1,21 @@
 from mpi4py import MPI
+import numpy
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
 if rank == 0:
-    with open('rockyou.txt', 'r', encoding='latin-1') as dictionary:
-        data = dictionary.readlines()
-else:
-    data = None
+    # with open('rockyou.txt', 'r', encoding='latin-1') as dictionary:
+    #     data = dictionary.readlines()
+    complete_data = numpy.loadtxt('rockyou_smol.txt', dtype=str, delimiter='\n', encoding='latin-1')
+    chunks = numpy.array_split(complete_data, size, axis=0)
 
-data = comm.scatter(data, root=0)
+
+else:
+    chunks = None
+
+data = comm.scatter(chunks, root=0)
 print('rank', rank, 'has data:', len(data))
 
 # For windows pleps only:
