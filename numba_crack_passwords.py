e42d7153585dc125daed8606ce5b74bcaab2de0f
Jonas Burger <post@jonas-burger.de>
2020-08-02 17:30:41 +0200

    fixed all bugs und added all features

--- numba_crack_passwords.py
+++ numba_crack_passwords.py
@@ -6,4 +6,5 @@ import math
 from password_cracker import PasswordCracker
 
 cracker = PasswordCracker(['test'])
-cracker.crack_gpu(target_hash="098f6bcd4621d373cade4e832627b4f6")
+res = cracker.crack_gpu(target_hash="098f6bcd4621d373cade4e832627b4f6")
+print(res)
