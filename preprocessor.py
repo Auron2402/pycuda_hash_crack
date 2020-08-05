3a504d852ff117a2430bd80f52765979f218b6e2
christian_neuerer@web.de <Lu24ci02fer95>
2020-08-05 17:04:05 +0200

    why would we use pandas?

--- preprocessor.py
+++ preprocessor.py
@@ -23,20 +23,24 @@ def preprocess(filename):
     print(f'I have rank {rank} in a size {size} cluster.')
     # read file, split file in chunks
     if rank == 0:
-        # read data
-        unprocessed_data = pd.read_csv(f'wordlists/{filename}.txt', sep="\n", encoding='latin-1', header=None,
-                                       squeeze=True, error_bad_lines=False, quoting=3)
-
-        # split and cast to string
-        unprocessed_chunks = numpy.array_split(unprocessed_data.astype(dtype='str'), size)
-        del unprocessed_data  # free up memory
+        # # read data
+        # unprocessed_data = pd.read_csv(f'wordlists/{filename}.txt', sep="\n", encoding='latin-1', header=None,
+        #                                squeeze=True, error_bad_lines=False, quoting=3)
+        #
+        # # split and cast to string
+        # #unprocessed_chunks = numpy.array_split(unprocessed_data.astype(dtype='str'), size)
+        # #del unprocessed_data  # free up memory
+        myfile = open(f'wordlists/{filename}.txt', 'r', encoding='latin-1')
+        content = myfile.read().split('\n')
     else:
         unprocessed_chunks = None
+        unprocessed_data = None
+        content = None
 
     # scatter data to nodes
-    recv_data = BM.scatter(unprocessed_chunks, comm)
+    recv_data = BM.scatter(content, comm)
     # recv_data = comm.scatter(unprocessed_chunks, root=0)
-    del unprocessed_chunks  # free up memory
+    # del unprocessed_chunks  # free up memory
 
     # call jonas preprocessing
     processed_data = password_cracker.prepare_wordlist(recv_data)
